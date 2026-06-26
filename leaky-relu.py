4ed40d62b005f8ac5868bd64eaccf74669322b0c
datqg0 <datqg0@gmail.com>
2026-06-27 01:26:41 +0700

    update

---
+++ leaky-relu.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def leaky_relu(x, alpha=0.01):
+    """
+    Vectorized Leaky ReLU implementation.
+    """
+    # Write code here
+    shape = np.shape(x)
+    np_arr = np.array(x,dtype=float);
+    np_arr = np_arr.flatten();
+    for i in range (len(np_arr)):
+        tmp=np_arr[i];
+        if(tmp<0):
+            tmp=tmp*alpha
+        np_arr[i]=tmp;
+    x = np_arr.reshape(shape)
+    return x
--- sigmoid-numpy.py
+++ sigmoid-numpy.py
@@ -10,7 +10,7 @@ def sigmoid(x):
     shape = np.shape(x)
     arr = np.asarray(x,dtype=float)
     #first way
-    #rr = arr.flatten();
+    #arr = arr.flatten();
     #for i in range(len(arr)):
     #    arr[i]=1/(1+e**(-arr[i]))
     #arr = arr.reshape(shape)
